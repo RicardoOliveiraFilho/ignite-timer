a6e66bc9dcf82c5d648614b550eb039903d90b08
Ricardo Oliveira <ricardo.oliveira1307@gmail.com>
2022-07-14 11:10:09 -0300

    React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,16 @@ import { ThemeProvider } from 'styled-components'
 
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
-
-import { Button } from './components/Button'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-      Texto de teste para cor global!
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+
       <GlobalStyle />
     </ThemeProvider>
   )

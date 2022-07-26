15f512f1bca7d6a3c32a01e6aaff1ebd9d80a364
Ricardo Oliveira <ricardo.oliveira1307@gmail.com>
2022-07-26 11:04:47 -0300

    Entendendo contextos

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import { ThemeProvider } from 'styled-components'
+// import { ThemeProvider } from 'styled-components'
 
-import { defaultTheme } from './styles/themes/default'
-import { GlobalStyle } from './styles/global'
-import { Router } from './Router'
-import { BrowserRouter } from 'react-router-dom'
+// import { defaultTheme } from './styles/themes/default'
+// import { GlobalStyle } from './styles/global'
+// import { Router } from './Router'
+// import { BrowserRouter } from 'react-router-dom'
+import { Home } from './Home'
 
 export function App() {
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+  return <Home />
 
-      <GlobalStyle />
-    </ThemeProvider>
-  )
+  // return (
+  //   <ThemeProvider theme={defaultTheme}>
+  //     <BrowserRouter>
+  //       <Router />
+  //     </BrowserRouter>
+
+  //     <GlobalStyle />
+  //   </ThemeProvider>
+  // )
 }
